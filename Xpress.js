d938277e6c2bf740f47bb0b673e22fca33d598ba
mostafa <mostafahatem300@gmail.com>
2021-11-29 21:29:11 +0100

    database connection and model

--- Xpress.js
+++ Xpress.js
@@ -1,26 +1,24 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const app = express()
 
-const bodyParser = require("body-parser");
-const cors = require("cors");
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(cors());
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(cors())
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, X-Auth-Token, Accept"
-    );
-    next();
-});
-
-
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, X-Auth-Token, Accept'
+  )
+  next()
+})
 
 module.exports = {
-    app
+  app,
 }
--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 require('dotenv').config()
 const { app } = require('./Xpress')
+const mongoose = require('mongoose')
 const port = process.env.PORT || 3000
-
-app.listen(port, () => {
-    console.log('Running --> Application on ' + port)
+mongoose
+  .connect(process.env.databaseURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
   })
-  
+  .then((db) => console.log('db is connected'))
+  .catch((err) => console.log(err))
+app.listen(port, () => {
+  console.log('Running --> Application on ' + port)
+})
